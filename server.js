9cb68b0b1ee18dac9c02e2ccbb9295683f34e72f
Puttipong Chomchit <kepler142593@gmail.com>
2023-10-27 12:51:19 +0700

    fix server

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.post("/", express.urlencoded({ extended: true }), (req, res, next) => {
                 let time = dry[id]['mode'][i]['time'] * 60 * 1000; // 30 minutes in milliseconds
                 let currentDate = new Date();
                 let newDate = new Date(currentDate.getTime() + time);
-                console.log(i + " : " +id);
+                console.log(i + " : " + id);
 
                 dry[id]['time'] = dry[id]['mode'][i]['time'];
                 dry[id]['timeout'] = newDate;//Oct 27, 2023 9:40:00
@@ -116,32 +116,6 @@ app.get("/dry", (req, res, next) => {
     res.sendFile(__dirname + "/selectMod.html")
 })
 */
-app.get("/api/v1/test/:myparams", (req, res, next) => {
-    let n = +req.params.myparams;
-    if (1 <= n && n <= 100 && Number.isInteger(n)) {
-        let fn = [];
-        let i = 1;
-        let n1 = 1;
-        let n2 = 1;
-        for (let j = 0; j < n; j++) {
-            if (j < 2) {
-                fn.push(j);
-            } else {
-                fn.push(i);
-                i = n1 + n2;
-                n1 = n2;
-                n2 = i;
-            }
-        }
-        res.json({
-            "member-count": req.params.myparams,
-            "sequence": [fn],
-            "total": fn.reduce((a, b) => a + b, 0)
-        })
-    } else {
-        res.status(413).send({ error: 'Parameter is not Valid Number!' });
-    }
-})
 
 app.listen(3000, () => {
     console.log('this server port 3000')
